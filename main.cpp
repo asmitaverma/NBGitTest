0110eea56944459e8447a32a5f82fd440a0a29aa
asmitaverma <asmitav65@gmail.com>
2020-04-04 21:46:47 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Asmita
+ *
+ * Created on 4 April, 2020, 9:28 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"hey there";
+    return 0;
+}
+
